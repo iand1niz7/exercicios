e5f1e85eddcb54d54e8e592a9e28e1ceaf0bb5be
iand1niz7 <ian.gabriel.diniz.nogueira@ccc.ufcg.edu.br>
2026-02-12 01:08:35 -0300

    implementando rotaçoes.

--- Rotacoes/src/rotacaoBST.java
+++ Rotacoes/src/rotacaoBST.java
@@ -39,4 +39,22 @@ class rotacaoBST{
     public void insert(int value){
         root = inserir(root, value);
     }
+    public int altura(Node n){
+    	if(n == null) return -1;
+	int alturaEsq = altura (n.left);
+	int alturaDir = altura (n.right);
+	return 1 + Math.max(alturaEsq, alturaDir);	
+    }
+    public int balance (Node n){
+    	if(n == null) return 0;
+	return altura(n.left) - altura(n.right);
+    }
+    public Node rotateRight(Node n){
+    	Node pivot = n.left;
+	if(balance(n) > 1) {
+	    pivot = n.parent;
+	    n.parent = pivot.right;
+	    pivot = parent;
+	}
+    }
 }
